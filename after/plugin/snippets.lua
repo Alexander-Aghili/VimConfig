994862b9527e9f4fd4e8c51a2e23b96a7d86790a
Alexander Aghili <awaghili@ucsc.edu>
2023-12-13 20:20:45 -0800

    Snippet working

--- after/plugin/snippets.lua
+++ after/plugin/snippets.lua
@@ -1,10 +1,58 @@
 local ls = require("luasnip")
+-- some shorthands...
 local s = ls.snippet
+local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
 local d = ls.dynamic_node
+local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.conditions")
+local conds_expand = require("luasnip.extras.conditions.expand")
 
+-- Configuration settings
+ls.setup({
+    keep_roots = true,
+    link_roots = true,
+    link_children = true,
+    update_events = "TextChanged,TextChangedI",
+    delete_check_events = "TextChanged",
+    ext_opts = {
+        [types.choiceNode] = {
+            active = {
+                virt_text = { { "choiceNode", "Comment" } },
+            },
+        },
+    },
+    ext_base_prio = 300,
+    ext_prio_increase = 1,
+    enable_autosnippets = true,
+    store_selection_keys = "<Tab>",
+})
+-- 'recursive' dynamic snippet. Expands to some text followed by itself.
+local rec_ls
+rec_ls = function()
+	return sn(
+		nil,
+		c(1, {
+			-- Order is important, sn(...) first would cause infinite loop of expansion.
+			t(""),
+			sn(nil, { t({ "", "\t\\item " }), i(1), d(2, rec_ls, {}) }),
+		})
+	)
+end
 
+-- Latex
 ls.add_snippets("tex", {
     s("ls", {
         t({ "\\begin{itemize}", "\t\\item " }),
@@ -15,3 +63,5 @@ ls.add_snippets("tex", {
 }, {
     key = "tex",
 })
+
+
--- lua/alex/remap.lua
+++ lua/alex/remap.lua
@@ -48,3 +48,11 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
+vim.cmd[["
+    imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+    smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>' 
+    
+    imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+    smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+
